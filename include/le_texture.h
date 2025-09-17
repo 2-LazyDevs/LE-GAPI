bf196fd6be1533798ae9405d9db1ecd96e9ce4bd
AbdulRehman <ardev1.deverson@proton.me>
2025-09-17 15:46:57 +0500

    Added & separated high & low-level API's
    This includes:
    - Added a high-level API which use stb_image
    - Separated it from the low-level API
    
    
    Signed-off-by: AbdulRehman <ardev1.deverson@proton.me>

--- include/le_texture.h
+++ include/le_texture.h
@@ -15,7 +15,17 @@
 
 #include "le_core.h"
 
+/* TODO: MAKE THE LOW-LEVEL API BETTER */
+// Low-level API (manual RGBA)
+// Can be primarly used by people who want manual control over texture creation & destruction
+
 int le_crtext(LE_C *ctx, int w, int h, const void *rgba_bytes);
 void le_desttext(LE_C *ctx, uint32_t handle);
+
+// High-level API (loaded via stb_image)
+// Can be primarly used by people who just want to quickly load & use their texture
+
+int le_loadtext(LE_C *ctx, const char *filename);
+void le_freetext(LE_C *ctx, uint32_t handle);
 
 #endif
