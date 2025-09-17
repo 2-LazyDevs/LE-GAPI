eecdbd381dde31b53f221a9091f4b0504fa9d6c8
AbdulRehman <ardev1.deverson@proton.me>
2025-09-17 17:47:43 +0500

    Added a high-level API
    This includes:
    - Added a high-level API which use stb_image
    - Separated it from the low-level API
    
    
    Signed-off-by: AbdulRehman <ardev1.deverson@proton.me>

--- src/le_texture.c
+++ src/le_texture.c
@@ -6,8 +6,14 @@
 /*****************************************/
 
 #include "../include/le_texture.h"
+
+#define STB_IMAGE_IMPLEMENTATION
+#include "../include/thirdparty/stb_image.h"
+
 #include <stdlib.h>
 #include <string.h>
+
+// Low-level API (manual RGBA)
 
 int le_crtext(LE_C *ctx, int w, int h, const void *rgba_bytes){
     for(int i=0;i<ctx->tex_cap;i++){
@@ -21,9 +27,31 @@ int le_crtext(LE_C *ctx, int w, int h, const void *rgba_bytes){
     return 0;
 }
 
-void le_desttext(LE_C *ctx, uint32_t handle){
+void le_desttext(LE_C *ctx, uint32_t handle) {
     int i = (int)handle - 1;
-    if(i>=0 && i<ctx->tex_cap && ctx->tex_pixels[i]){
-        free(ctx->tex_pixels[i]); ctx->tex_pixels[i]=NULL;
+    if (i >= 0 && i < ctx->tex_cap && ctx->tex_pixels[i]) {
+        free(ctx->tex_pixels[i]);
+        ctx->tex_pixels[i] = NULL;
+        ctx->tex_w[i] = 0;
+        ctx->tex_h[i] = 0;
     }
+}
+
+// High-level API (loaded via stb_image)
+
+int le_loadtext(LE_C *ctx, const char *filename) {
+    int w, h, channels;
+    unsigned char *data = stbi_load(filename, &w, &h, &channels, 4);
+    if (!data) {
+        /* TODO: ADD A MESSSAGE BOX */
+        return -1;
+    }
+    
+    int texthand = le_crtext(ctx, w, h, data); // Texture handler
+    stbi_image_free(data);
+    return texthand;
+}
+
+void le_freetext(LE_C *ctx, uint32_t texthand) {
+    le_desttext(ctx, texthand)
 }
